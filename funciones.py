47c3f0c5de3e0cac3565cde552b4ae47efc5d5b7
spinelf@gmail.com <spinelf@gmail.com>
2024-06-09 22:20:55 +0200

    subida de reestructuracion de carpetas y comentarios de las funciones.

--- funciones.py
+++ funciones.py
@@ -7,13 +7,15 @@ from IPython.display import display
 import scipy.stats as stats
 from scipy.stats import chi2_contingency, ttest_ind
 
-def cargar_csv(url):
+def cargar_csv(url):  #Función que nos permite cargar el csv para crear el DataFrame
     
     df = pd.read_csv(url)
     
     return df
 
-def propiedades(df):
+def propiedades(df):  
+    
+    ''' Funcion que nos indica las distintas propiedades del Dataframe que recibe como parámetro. '''
     
     #Vemos las primeras lineas del DataFrame
     
@@ -22,6 +24,7 @@ def propiedades(df):
     
     
     #Vemos las cantidad de filas y columnas del DataFrame Vuelos
+    
     print(f"El Dataframe de tiene {df.shape[0]} filas y {df.shape[1]} columnas")
     
     print("***************************************************")
@@ -39,12 +42,15 @@ def propiedades(df):
     print(f"Si vemos los nulos en forma de porcentaje:")
     
     # lo primero que vamos a hacer es calcular el porcentaje de nulos que tenemos para cada columna
+    
     porc_nulos = (df.isnull().sum() / df.shape[0]) * 100
     
     # lo convertimos a DataFrame
+    
     df_nulos = pd.DataFrame(porc_nulos, columns = ["%_nulos"])
     
     #filtramos el DataFrame para quedarnos solo con aquellas columnas que tengan nulos
+    
     display(df_nulos[df_nulos["%_nulos"] > 0])
     
     
@@ -61,6 +67,7 @@ def propiedades(df):
     print("********************************************************************************")
     
     #Analizamos las columnas del Dataframe Vuelos
+    
     print("El listado de columnas es:")
     print("")
     
@@ -68,7 +75,7 @@ def propiedades(df):
     
     print("********************************************************************************")
     
-    for column in df.columns:
+    for column in df.columns:     #Nos muestra todas las columnas y los valores unicos de cada una de ellas.
   
         print(f"Columna: {column}")
         print(df[column].unique())
@@ -76,9 +83,23 @@ def propiedades(df):
     print("\n--------------------\n")
     
     
-def quitar_duplicados(df):
+def quitar_duplicados(df,sobreescribir):   
     
-    df.drop_duplicates(inplace=True)
+    ''' Función que nos permite quitar los duplicados en un DataFrame. Nota: Esta función elimina y sobreescribe el Dataframe 
+    original si le pasamos el parametro true y devuelve un nuevo DataFrame sino queremoms trabajar sobre el original.
+     Parámetros:
+          dataframe (DataFrame): El DataFrame que contiene los datos.
+          sobreescribir (str): Toma el valor True o False según queremos o no modificar el dataframe original.
+    '''
+    
+    if sobreescribir == True:
+        
+        df.drop_duplicates(inplace=sobreescribir)
+        return df
+    
+    else:
+        df_no_duplicados= df.drop_duplicates(inplace=sobreescribir)
+        return df_no_duplicados
     
     #Comprobamos que se han eliminado todos los duplicados
     
@@ -86,31 +107,47 @@ def quitar_duplicados(df):
     
     return df
 
-def cambiar_valores_NaN (df,columna):
+def cambiar_valores_NaN (df,columna,valor): 
     
-    df[columna] = df[columna].fillna(0)
+    ''' Función con la que rellenamos los valores NaN de una columna con el valor que queramos, para ello lo pasamos por parámetro.
+        Parámetros:
+          dataframe (DataFrame): El DataFrame que contiene los datos.
+          columna (str): El nombre de la columna en el DataFrame que se va a sustituir.
+          valor (str) Valor por el que se van a sustituir los valores NaN'''
+    
+    df[columna] = df[columna].fillna(valor)
     return df
 
 
-def ver_duplicados(df):
+def ver_duplicados(df):   
+    
+    '''Esta función nos permite ver los duplicados de un DataFrame.'''
     
     for col in df.columns:
         
         print(f"Los duplicados de la columna {col} son: {df[col].duplicated().sum()}")
         
 
-def conversion_tipos(df,columna,tipo_destino):
+def conversion_tipos(df,columna,tipo_destino):  
     
-   # Convierte la columna al tipo indicado
-        df[columna] = df[columna].astype(tipo_destino)
+    ''' Con esta función convertimos el valor de la columna al tipo que se indican en los parámetros.
     
-        return df[columna]
+        Parámetros:
+        dataframe (DataFrame): El DataFrame que contiene los datos.
+        columna (str): El nombre de la columna en el DataFrame que se va a convertir.
+        tipo_destino (str) El tipo de dato al que vamos a convertir la columna'''
+    
+    # Convierte la columna al tipo indicado
+    df[columna] = df[columna].astype(tipo_destino)
+    
+    return df[columna]
 
-def fusionar_dfs(df1,df2,columna,como):
+def fusionar_dfs(df1,df2,columna,como):   # Esta función nor permite fusionar dos Dataframe.
     
     return pd.merge(df1, df2, on=columna, how=como)
     
 def normalidad(dataframe, columna):
+    
     """
     Evalúa la normalidad de una columna de datos de un DataFrame utilizando la prueba de Shapiro-Wilk.
 
@@ -166,7 +203,7 @@ def test_man_whitney(columna1,columna2):
     Se utiliza la prueba de Mann-Whitney U para evaluar si hay diferencias significativas entre los grupos.
     """
           
-        # aplicamos el estadístico
+    # aplicamos el estadístico
     statistic, p_value = stats.mannwhitneyu(columna1,columna2)
                
     if p_value < 0.05:
